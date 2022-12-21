4025e891676bb218866ade803b9ef6c1a2c349c2
José Dagmar Florentino da Silva Sobrinho <64164006+Dagmar87@users.noreply.github.com>
2022-12-21 07:54:05 -0300

    Criação do projeto até a finalização da etapa do email.php

---
+++ email.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+    
+    $nome = addslashes($_POST['name']);
+    $email = addslashes($_POST['email']);
+    $mensagem = addslashes($_POST['message']);
+    
+    $to = "contato@programador87.com";
+    $subject = "Contato - Programador 87";
+    $body = "Nome: ".$nome. "\r\n".
+            "Email: ".$email. "\r\n".
+            "Mensagem: ".$mensagem;
+    $header = "From: jdfssobrinho@programador87.com"."\r\n"
+            ."Reply-To:".$email."\r\n"
+            ."X=Mailer:PHP/".phpversion();
+    
+    if(mail($to,$subject,$body,$header)){
+        echo("Email enviado com sucesso");
+    } else {
+        echo("O Email não pode ser enviado");
+    }    
+}
+
+?>
+
+
+
